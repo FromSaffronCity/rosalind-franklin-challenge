2f3def7df3f3f39d57d7d0ab744ac7059997db6d
Ajmain Yasar Ahmed Sahil <39818981+FromSaffronCity@users.noreply.github.com>
2020-12-21 15:10:45 +0600

    bioinformatics_textbook_track.py updated

--- bioinformatics-textbook-track/bioinformatics_textbook_track.py
+++ bioinformatics-textbook-track/bioinformatics_textbook_track.py
@@ -4,3 +4,17 @@ def compute_the_hamming_distance_between_two_strings(dna_string_1, dna_string_2)
         if dna_string_1[i] != dna_string_2[i]:
             hamming_distance += 1
     return hamming_distance
+
+
+def find_the_reverse_complement_of_a_string(dna_string):
+    reverse_complement_dna_string = ""
+    for char in dna_string:
+        if char == 'A':
+            reverse_complement_dna_string = 'T' + reverse_complement_dna_string
+        elif char == 'T':
+            reverse_complement_dna_string = 'A' + reverse_complement_dna_string
+        elif char == 'G':
+            reverse_complement_dna_string = 'C' + reverse_complement_dna_string
+        elif char == 'C':
+            reverse_complement_dna_string = 'G' + reverse_complement_dna_string
+    return reverse_complement_dna_string
