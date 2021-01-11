feee5f4b684b34fc0b450dc012e0df00e2cbd01a
Ajmain Yasar Ahmed Sahil <39818981+FromSaffronCity@users.noreply.github.com>
2021-01-11 13:06:16 +0600

    +find_all_occurrences_of_a_pattern_in_a_string()

--- bioinformatics-textbook-track/bioinformatics_textbook_track.py
+++ bioinformatics-textbook-track/bioinformatics_textbook_track.py
@@ -18,3 +18,14 @@ def find_the_reverse_complement_of_a_string(dna_string):
         elif char == 'C':
             reverse_complement_dna_string = 'G' + reverse_complement_dna_string
     return reverse_complement_dna_string
+
+
+def find_all_occurrences_of_a_pattern_in_a_string(pattern, genome):
+    starting_positions = []
+    next_starting_position = 0
+
+    while next_starting_position != -1:
+        next_starting_position = genome.find(pattern, (next_starting_position+1 if next_starting_position != 0 else 0))
+        if next_starting_position != -1:
+            starting_positions.append(next_starting_position)
+    return starting_positions
